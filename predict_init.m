3a8032a13f90c70c4192c365b51b1ecbf2d4d811
iglesias <fernando.iglesiasg@gmail.com>
2011-12-14 12:27:19 +0100

    + predict step when there is no previous shared information, section III. A) Propagation in the paper

---
+++ predict_init.m
@@ -0,0 +1,25 @@
+% function [mu_bar, sigma_bar] = predict(mu, sigma, u, R)
+% This function performs the prediction step when there is no previous shared
+% measurements between the robots.
+% Inputs:
+%           mu(t-1)           3X1   
+%           sigma(t-1)        3X3
+%           u(t)              3X1
+%           R                 3X3
+% Outputs:   
+%           mu_bar(t)         3X1
+%           sigma_bar(t)      3X3
+function [mu_bar, sigma_bar] = predict(mu, sigma, u, R)
+
+g = mu+u;   % Linearization
+G = [
+        1   0   -u(2);
+        0   1    u(1);
+        0   0       1
+    ];      % Jacobian of g
+
+% Prediction step
+mu_bar = g;
+sigma_bar = G*sigma*G' + R;
+
+end
