7b9dcd5545f153b762d357c615e02a9e0fe13b82
iglesias <fernando.iglesiasg@gmail.com>
2011-12-23 01:19:22 +0100

    ~ modified to the robot structure

--- predict_init.m
+++ predict_init.m
@@ -1,25 +1,22 @@
 % function [mu_bar, sigma_bar] = predict_init(mu, sigma, u, R)
-% This function performs the prediction step when there is no previous shared
+% This function performs the prediction step when there are no previous shared
 % measurements between the robots.
 % Inputs:
-%           mu(t-1)           3X1   
-%           sigma(t-1)        3X3
-%           u(t)              3X1
-%           R                 3X3
+%           robot(t):         robot structure   
+%           Q:                3X3
 % Outputs:   
-%           mu_bar(t)         3X1
-%           sigma_bar(t)      3X3
-function [mu_bar, sigma_bar] = predict_init(mu, sigma, u, Q)
+%           robot(t):         robot structure
+function robot = predict_init(robot, Q)
 
-g = mu + u;   % Linearization
+g = robot.mu + robot.u;   % Linearization
 G = [
-        1   0   -u(2);
-        0   1    u(1);
+        1   0   -robot.u(2);
+        0   1    robot.u(1);
         0   0       1
-    ];        % Jacobian of g
+    ];                    % Jacobian of g
 
 % Prediction step
-mu_bar    = g;
-sigma_bar = G*sigma*G' + Q;
+robot.mu_bar    = g;
+robot.sigma_bar = G*robot.sigma*G' + Q;
 
 end
