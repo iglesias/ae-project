121d462d178c6be59528ca397df43996c0efde83
iglesias <fernando.iglesiasg@gmail.com>
2011-12-25 16:57:29 +0100

    + more attributes in robot initialization

--- init_robot.m
+++ init_robot.m
@@ -1,10 +1,18 @@
 % function robot = init_robot()
 % Initializes a robot structure
+%
 % Outputs:
 %           robot:    robot structure
+%
+% Robot attributes:
+%           P_ita:    each robot needs to track its prediction steps to later
+%                     predict the cross correlation terms
+%
 function robot = init_robot()
 
-robot.mu    = zeros(3, 1);            % Initial estimate of state
-robot.sigma = 1e-10*diag( [1 1 1] );  % Initial covariance matrix
+robot.mu          = zeros(3, 1);            % Initial estimate of state
+robot.sigma       = 1e-10*diag( [1 1 1] );  % Initial covariance matrix
+robot.lastUpdate  = false;             
+robot.P_ita       = zeros(3, 3);
 
 end
