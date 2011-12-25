7b83dae06d9879f2c3aacc389132b3ca7990c2a2
iglesias <fernando.iglesiasg@gmail.com>
2011-12-25 16:59:17 +0100

    * various fixes

--- ekf_localize.m
+++ ekf_localize.m
@@ -1,6 +1,6 @@
-% function [robot, outliers] = ekf_localize(robot, R, Q, z, known_assoc, M,
-%                                           Lambda_M, map_ids_true, t)
-% This function should perform one iteration of EKF localization using batch
+% function [robot, outliers] = ekf_localize(robot, R, Q, z, known_asso, ...
+%                                           M, lambda_m, MAP_IDS, t)
+% This function performs one iteration of EKF localization using batch
 % update.
 %
 % Inputs:
@@ -8,7 +8,7 @@
 %           R:                    3X3
 %           Q:                    2X2
 %           z:                    2Xn
-%           known_assoc:          1Xn
+%           known_asso:           1Xn
 %           M:                    2XN
 %           Lambda_M:             1X1
 %           map_ids_true:         1xN
@@ -17,40 +17,42 @@
 %           robot:               robot structure
 %           outliers:            1X1
 %
-function [robot, outliers] = ekf_localize(robot, R, Q, z, known_assoc, M, Lambda_M, ...
-                                          map_ids_true, t)
-
-% TODO decide which predict is the one to be used
+function [robot, outliers] = ekf_localize(robot, R, Q, z, known_asso, ...
+                                          M, lambda_m, MAP_IDS, t)
+                                        
 robot = predict(robot, R);
 
-n         = size(z, 2);
-outliers  = 0;
-
-i  = 0;
-
-% TODO adapt this to the robot structure
-[c, outlier, nu_bar, H_bar] = batch_associate(mu_bar, sigma_bar, z, M, ...
-                                              Lambda_M, Q);
+[c, outlier, nu_bar, H_bar] = batch_associate(robot, z, M, lambda_m, Q);
 
 if sum(outlier)
-  fprintf('warning, %d measurements were labelled as outliers, t = %d', ...
+  fprintf('%d measurements were labelled as outliers, t = %d', ...
           sum(outlier), t);
 end
 
-valid_idxs = find(~outlier);  % indices of inliers
-ix = [2*(valid_ixs-1)+1;2*(valid_ixs-1)+2];
-ix = ix(:); 
-nu_bar = nu_bar(ix);
-H_bar = H_bar(ix,:);
-n = length(valid_ixs);
-Q_bar = zeros(2*n,2*n);
-for i=1:n
-    ii= 2*i + (-1:0);
-    Q_bar(ii,ii) = Q;
+map_ids = zeros( 1, size(z, 2) );
+for i = 1:size(z, 2)
+  map_ids(i) = find( MAP_IDS == known_asso(i) );
+  if map_ids(i) ~= c(i)
+    fprintf('%dth measurement of landmark %d incorrectly associated ', ...
+            'to landmark %d, t = %d', i, map_ids(i), c(i), t);
+  end
 end
 
-% TODO adapth this to the robot structure
-[mu,sigma] = batch_update(mu_bar,sigma_bar,H_bar,Q_bar,nu_bar);   
+valid_ixs   = find(~outlier);  % indices of inliers
+ix          = [2*(valid_ixs - 1) + 1; 2*(valid_ixs - 1) + 2];
+ix          = ix(:);
+
+n       = length(valid_ixs);
+nu_bar  = nu_bar(ix);
+H_bar   = H_bar(ix, :);
+Q_bar   = zeros(2*n, 2*n);
+for i = 1:n
+    ii = 2*i + (-1:0);
+    Q_bar(ii, ii) = Q;
+end
+
+robot = batch_update(robot, H_bar, Q_bar, nu_bar);
+
 outliers = sum(outlier);
 
 end
