796775a6b5a2d63fbf5423614651aac7e82245fe
iglesias <fernando.iglesiasg@gmail.com>
2011-12-25 16:15:46 +0100

    * typos in run_cl

--- run_cl.m
+++ run_cl.m
@@ -21,10 +21,10 @@ robot1 = init_robot();
 robot2 = init_robot();
 
 % Miscellaneous
-sensorpose      = zeros(3, 1);
-odom            = zeros(3, 1)
-enc             = zeros(2, 1);
+% sensorpose      = zeros(3, 1);
 total_outliers  = 0;
+enc1            = zeros(1, 2);
+enc2            = zeros(1, 2);
 t               = 0;
 
 %% Figures Initialization
@@ -32,7 +32,7 @@ t               = 0;
 dataset_basedir = 'Datasets/';
 margin          = 5;
 
-d = load( [dataset_basedir mapfile] )
+d = load( [dataset_basedir mapfile] );
 
 if 1  % TODO add verbose??
 
@@ -54,7 +54,6 @@ end
 
 % ??
 
-hcovs = [];
 if 1  % TODO add verbose??
   figure(mapfig);
   hcovs = plot(0, 0, 'r', 'erasemode', 'xor');
@@ -68,12 +67,12 @@ fid1    = fopen( [dataset_basedir simoutfile1], 'r' );
 fid2    = fopen( [dataset_basedir simoutfile2], 'r' );
 
 if fid1 <= 0
-  fprintf('Failed to open simoutput file "s"\n', simoutfile);
+  fprintf('Failed to open simoutput file %s\n', simoutfile1);
   return
 end
 
 if fid2 <= 0
-  fprintf('Failed to open simoutput file "s"\n', simoutfile);
+  fprintf('Failed to open simoutput file %s\n', simoutfile2);
   return
 end
 
@@ -83,7 +82,7 @@ while 1
   if ~ischar(line)
     break
   end
-  flines1 = {flines1{:} line};
+  flines1 = [flines1{:} line];
 end
 
 flines2 = {};
@@ -92,7 +91,7 @@ while 1
   if ~ischar(line)
     break
   end
-  files2 = {flines{:} line}
+  flines2 = [flines2{:} line];
 end
 
 fclose(fid1);
@@ -111,11 +110,11 @@ while i < min( length(flines1), length(fline2) )
 
   pt1          = t;
   t1           = values(1);
-  delta_t1     = t - pt;
+  delta_t1     = t1 - pt1;
   odom1        = values(2:4);
-  penc1        = enc;
+  penc1        = enc1;
   enc1         = values(5:6);
-  denc1        = enc - penc;
+  denc1        = enc1 - penc1;
   truepose1    = values(7:9);
 
   n = values(10);
@@ -130,17 +129,16 @@ while i < min( length(flines1), length(fline2) )
   end
 
   % Read robot2's data
-
+ 
   line    = flines2{i};
   values  = sscanf(line, '%f');
-
   pt2          = t;
   t2           = values(1);
-  delta_t2     = t - pt;
+  delta_t2     = t2 - pt2;
   odom2        = values(2:4);
-  penc2        = enc;
+  penc2        = enc2;
   enc2         = values(5:6);
-  denc2        = enc - penc;
+  denc2        = enc2 - penc2;
   truepose2    = values(7:9);
 
   % Compute the control signals of the robots
@@ -162,15 +160,12 @@ while i < min( length(flines1), length(fline2) )
   if n > 0
 
     plot(robot1.mu(1), robot1.mu(2), 'rx')
-    RE = [cos(robot1.mu(3)) -sin(robot1.mu(3)); 
-          sin(robot1.mu(3))  cos(robot1.mu(3))];
-
-    xsE = robot1.mu(1:3) + [RE * sensorpose(1:2); sensorpose(3)];
 
     pcov = make_covariance_ellipses(robot1.mu, robot1.sigma);
     set(hcovs, 'xdata', pcov(1, :), 'ydata', pcov(2,:));
     title( sprintf('t = %d, total outliers = %d, current outliers = %d', ...
                     i, total_outliers, outliers) );
+                
     axis( [xmin xmax ymin ymax] ) 
 
   end
@@ -178,12 +173,8 @@ while i < min( length(flines1), length(fline2) )
   % Plot the true pose
   if n > 0        
 
-    plot(truepose(1), truepose(2), 'gx');
-    RG = [cos(truepose(3)) -sin(truepose(3)); 
-          sin(truepose(3))  cos(truepose(3))];
-
-    xsG = truepose(1:3) + [RG * sensorpose(1:2); sensorpose(3)];
-
+    plot(truepose1(1), truepose1(2), 'gx');
+    
     axis([xmin xmax ymin ymax]) 
 
   end
@@ -191,12 +182,8 @@ while i < min( length(flines1), length(fline2) )
   % Plot the odometry
   if n > 0
 
-    plot(odom(1), odom(2), 'bx');
-    RO = [cos(odom(3)) -sin(odom(3)); 
-          sin(odom(3))  cos(odom(3))];
-   
-    xsO = odom(1:3) + [RO * sensorpose(1:2); sensorpose(3)];
-
+    plot(odom1(1), odom1(2), 'bx');
+    
     axis([xmin xmax ymin ymax]) 
 
   end
