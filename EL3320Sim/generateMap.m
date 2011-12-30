b342650135be1208641309e20284e57409d70858
iglesias <fernando.iglesiasg@gmail.com>
2011-12-30 01:14:05 +0100

    + generateMap handles generation of several trajectories

--- EL3320Sim/generateMap.m
+++ EL3320Sim/generateMap.m
@@ -1,21 +1,26 @@
 %
 % Function that makes it easy to create landmarks for a map as well as 
-% a trajectory for the robot to drive.
+% several trajectories for the robots to drive.
 %
 % Authors: Patric Jensfelt, 2008
 %          Alper Aydemir, 2009
-%
+%          Fernando J. Iglesias García, 2011
 
 function posLandm=generateMap()
 
 clf
-addMore = 1;
-posLandm = [];
-trajectory = [];
+addMore   = 1;
+posLandm  = [];
+trajs     = {};
+trajs{1}  = [];
+trajIdx   = 1;   % Index for the current trajectory
+
 disp('Left-click in figure to add a landmark')
-disp('Mid/right-click in figure to add a way point in the trajectory')
+disp('Right-click in figure to add a way point in the trajectory')
+disp('Mid-click in figure to start a new trajectory')
 disp('Press ENTER once to display what you got so far')
 disp('Press ENTER twice to end input of landmarks')
+
 while addMore
     axis([-10 10 -10 10].*2)
     [x y button] = ginput();
@@ -25,21 +30,24 @@ while addMore
         for i=1:length(x)
             if button(i) == 1
                 posLandm = [posLandm; [x(i) y(i)] ];
-            else
-                trajectory = [trajectory; [x(i) y(i)] ];
+            elseif button(i) == 2
+                trajIdx         = trajIdx + 1;
+                trajs{trajIdx}  = [];
+            elseif button(i) == 3
+                trajs{trajIdx} = [trajs{trajIdx}; [x(i) y(i)] ];
             end
         end
     end
     hold on
     if ~isempty(posLandm)
-        h = plot(posLandm(:,1), posLandm(:,2),'ro');
+        plot(posLandm(:,1), posLandm(:,2),'ro');
     end
-    if ~isempty(trajectory)
-         for i=1:length(trajectory)
-             h = plot(trajectory(i,1), trajectory(i,2),'bx');
+    if ~isempty( trajs{trajIdx} )
+         for i = 1:length( trajs{trajIdx} )
+             h = plot(trajs{trajIdx}(i,1), trajs{trajIdx}(i,2),'bx');
              str = strcat('\leftarrow   ', num2str(i)); 
-             text(trajectory(i,1), trajectory(i,2),str);
-             legend(h,'way points');
+             text(trajs{trajIdx}(i,1), trajs{trajIdx}(i,2), str);
+             legend(h, 'way points');
          end
     end
 end
@@ -48,17 +56,22 @@ if ~isempty(posLandm)
     posLandm = [(1:size(posLandm,1))' posLandm];
     fid = fopen('map.txt', 'w');
     for k = 1:size(posLandm,1)
-        fprintf(fid, '%d %.3f %.3f\n', posLandm(k,1), posLandm(k,2), posLandm(k,3));
+        fprintf(fid, '%d %.3f %.3f\n', ...
+                posLandm(k,1), posLandm(k,2), posLandm(k,3));
     end
     fclose(fid);
 end
 
-if ~isempty(trajectory)
-    fid = fopen('traj.txt', 'w');
-    for k = 1:length(trajectory)
-        fprintf(fid, '#Goto (%.3f,%.3f) with %.1fm tolerance\n', trajectory(k,1), trajectory(k,2),0.1);
-        fprintf(fid, '%d %.3f %.3f %.3f\n\n', 1, trajectory(k,1), trajectory(k,2),0.1);
+if ~isempty(trajs)
+    for i = 1:length(trajs)
+        fid = fopen(['traj' sprintf('%d', i) '.txt'], 'w');
+        for k = 1:length(trajs{i})
+            fprintf(fid, '#Goto (%.3f,%.3f) with %.1fm tolerance\n', ...
+                    trajs{i}(k, 1), trajs{i}(k, 2), 0.1);
+            fprintf(fid, '%d %.3f %.3f %.3f\n\n', 1, ...
+                    trajs{i}(k, 1), trajs{i}(k, 2), 0.1);
+        end
+        fclose(fid);
     end
-    fclose(fid);
 end
 
